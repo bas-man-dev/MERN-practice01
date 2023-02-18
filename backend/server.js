4a18385baf72f9b3fa2d953f03fb4191e7d5bf4d
Bas Man <bas.man.developer@gmail.com>
2023-02-18 19:06:53 +0000

    updated middleware, checked postman GET request

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
 
 // Creates the express app
 const app = express();
+
+// Middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 // routes
 app.get('/', (req, res) => {
@@ -9,6 +17,6 @@ app.get('/', (req, res) => {
 });
 
 // Listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('listening on port 4000');
 });
