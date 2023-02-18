f2e4f95f7f3419fa0f5d2f8b2a103d0e75158a73
Bas Man <bas.man.developer@gmail.com>
2023-02-18 21:57:30 +0000

    added mongo db and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
 require('dotenv').config()
 
-const express = require('express');
-const workoutRoutes = require('./routes/workouts');
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+// fixed an  error here: https://stackoverflow.com/questions/74711770/how-to-fix-mongoose-deprecation-warning-the-strictquery
+mongoose.set('strictQuery', false);
+// express app
+const app = express()
 
-// Creates the express app
-const app = express();
-
-// Middleware
-app.use(express.json());
+// middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-});
+  console.log(req.path, req.method)
+  next()
+})
 
 // routes
-app.use('/api/workouts', workoutRoutes);
+app.use('/api/workouts', workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000');
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
